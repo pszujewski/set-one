afe683a42ce6fc476ff7d564babb313a72e93664
Peter Szujewski <pszujewski@gmail.com>
2017-02-27 13:03:23 -0500

    fourth commit

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 function whoAmI(name, age) {
+  var yearOfBirth = 2017 - age;
   console.log(`hi, my name is ${name} and I am ${age} years old.`);
+  console.log(`I was born in ${yearOfBirth}.`);
 }
 whoAmI("Peter", "27");
