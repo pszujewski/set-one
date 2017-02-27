92ed917cc6d047d9e84a5d70122391438653ee37
Peter Szujewski <pszujewski@gmail.com>
2017-02-27 13:20:10 -0500

    almost finishedwith set one

--- js/app.js
+++ js/app.js
@@ -7,16 +7,18 @@ function whoAmI(name, age) {
   if (name === undefined || age === undefined) {
     console.error("Arguments not valid");
   }
-  try {
-    if (age < 0) {
-      throw new Error("Hey man, you can't use negatives!");
+  else if (typeof name === "string" && isNaN(age) === false) {
+    try {
+      if (age < 0) {
+        throw new Error("Hey man, you can't use negatives!");
+      }
+      var yearOfBirth = 2017 - age;
+      console.log(`hi, my name is ${name} and I am ${age} years old.`);
+      getYearOfBirth(age);
     }
-    var yearOfBirth = 2017 - age;
-    console.log(`hi, my name is ${name} and I am ${age} years old.`);
-    getYearOfBirth(age);
-  }
-  catch(err) {
-    console.log(err.message);
+    catch(err) {
+      console.log(err.message);
+    }
   }
 }
-whoAmI("Peter", "27");
+whoAmI("Peter", "nine");
