1f470b1c630d105ffcb0d235da14311c88a54739
Peter Szujewski <pszujewski@gmail.com>
2017-02-27 14:05:33 -0500

    Used es6 syntaxand finished first draft

--- js/app.js
+++ js/app.js
@@ -1,24 +1,22 @@
 function getYearOfBirth(age) {
-  var yearOfBirth = 2017 - age;
+  let yearOfBirth = 2017 - age;
   console.log(`I was born in ${yearOfBirth}.`);
 }
 
 function whoAmI(name, age) {
-  if (name === undefined || age === undefined) {
+  if (name === undefined || age === undefined || typeof name !== "string" || isNaN(age)) {
     console.error("Arguments not valid");
   }
-  else if (typeof name === "string" && isNaN(age) === false) {
-    try {
-      if (age < 0) {
-        throw new Error("Hey man, you can't use negatives!");
-      }
-      var yearOfBirth = 2017 - age;
+  try {
+    if (age < 0) {
+      throw new Error("Hey man, you can't use negatives!");
+    } else {
       console.log(`hi, my name is ${name} and I am ${age} years old.`);
       getYearOfBirth(age);
     }
-    catch(err) {
-      console.log(err.message);
-    }
+  }
+  catch(err) {
+    console.log(err.message);
   }
 }
-whoAmI("Peter", "nine");
+whoAmI("Peter", "9");
