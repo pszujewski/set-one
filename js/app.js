3be0cc0aab6bd286110ee4608076ca6aab9f69a3
Peter Szujewski <pszujewski@gmail.com>
2017-02-27 13:11:04 -0500

    Added try catch blocks

--- js/app.js
+++ js/app.js
@@ -4,8 +4,16 @@ function getYearOfBirth(age) {
 }
 
 function whoAmI(name, age) {
-  var yearOfBirth = 2017 - age;
-  console.log(`hi, my name is ${name} and I am ${age} years old.`);
-  getYearOfBirth(age);
+  try {
+    if (age < 0) {
+      throw new Error("Hey man, you can't use negatives!");
+    }
+    var yearOfBirth = 2017 - age;
+    console.log(`hi, my name is ${name} and I am ${age} years old.`);
+    getYearOfBirth(age);
+  }
+  catch(err) {
+    console.log(err.message);
+  }
 }
 whoAmI("Peter", "27");
