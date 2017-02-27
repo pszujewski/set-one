90eeaea1a8a7ab816df9cc910558e57576fc119f
Peter Szujewski <pszujewski@gmail.com>
2017-02-27 13:06:55 -0500

    added function getYearOfBirth

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
+function getYearOfBirth(age) {
+  var yearOfBirth = 2017 - age;
+  console.log(`I was born in ${yearOfBirth}.`);
+}
+
 function whoAmI(name, age) {
   var yearOfBirth = 2017 - age;
   console.log(`hi, my name is ${name} and I am ${age} years old.`);
-  console.log(`I was born in ${yearOfBirth}.`);
+  getYearOfBirth(age);
 }
 whoAmI("Peter", "27");
